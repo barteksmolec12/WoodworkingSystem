b2144d7de3af6e2638e60ecc1d03f10189def6c3
barteksmolec12 <barteksmolec@o2.pl>
2020-11-22 11:20:22 +0100

    Create JoineryOrderDetails model

---
+++ Data/JoineryOrderDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Data
+{
+	public class JoineryOrderDetails
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public int JoineryOrderId { get; set; }
+		[ForeignKey("JoineryOrderId")]
+		public virtual JoineryOrder JoineryOrder { get; set; }
+		[Required]
+		public int ProductDetailsId  { get; set; }
+		[Required]
+		public int WoodmakerId { get; set; }
+		[Required]
+		public string Status  { get; set; }
+	}
+}
