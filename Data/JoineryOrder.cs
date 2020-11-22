15d31a3043e74d8fc056aaefaacf7467000155f3
barteksmolec12 <barteksmolec@o2.pl>
2020-11-22 11:05:24 +0100

    Create JoineryOrder model

---
+++ Data/JoineryOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Data
+{
+	public class JoineryOrder
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public int UserId { get; set; }
+		[Required]
+		public DateTime DataOrder { get; set; }
+		public DateTime DateOrderEnd { get; set; }
+		[Required]
+		public decimal TotalPrice { get; set; }
+		[Required]
+		public string Status  { get; set; }
+	}
+}
