ea29985ac93939514cd18856785695c690bdcf59
barteksmolec12 <barteksmolec@o2.pl>
2020-11-22 12:03:46 +0100

    Create models: Product, Category

---
+++ Data/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Data
+{
+	public class Product
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string ProductName { get; set; }
+		[Required]
+		public string CategoryId  { get; set; }
+		[ForeignKey("CategoryId")]
+		public virtual Category Category { get; set; }
+		public string  Description { get; set; }
+		public string Image  { get; set; }
+		[Required]
+		public double Price  { get; set; }
+	}
+}
