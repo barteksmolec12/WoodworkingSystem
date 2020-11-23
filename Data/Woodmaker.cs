ff707886b7bdc765b2a1449540c16191f8496cc8
barteksmolec12 <barteksmolec@o2.pl>
2020-11-23 18:54:44 +0100

    Create models: Woodmaker,Machine

---
+++ Data/Woodmaker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Data
+{
+	public class Woodmaker
+	{
+		[Key]
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string SecondName { get; set; }
+	}
+}
