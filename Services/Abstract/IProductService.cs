d46c3e7e7c56a4024b9834bd74833de39cb1eb97
barteksmolec12 <barteksmolec@o2.pl>
2020-11-24 20:08:23 +0100

    Implementation IProductService

---
+++ Services/Abstract/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Abstract
+{
+	public interface IProductService
+	{
+		IEnumerable<Product> GetProducts();
+		void AddProduct(Product product);
+		void DeleteProduct(Product product);
+		void UpdateProduct(Product product);
+	}
+}
