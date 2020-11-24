33821b98b1fba7c5bf9e70ed3b0dfa2a39b183c7
barteksmolec12 <barteksmolec@o2.pl>
2020-11-24 19:44:59 +0100

    Implementation class Repository

--- Repository/IRepository.cs
+++ Repository/IRepository.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Repository
 {
-	public interface IRepository<T> where T:class
+	public interface IRepository<T> where T:BaseEntity
 	{
         T Get(int id);
         IEnumerable<T> GetAll();
---
+++ Repository/Repository.cs
@@ -0,0 +1,78 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+	public class Repository<T> : IRepository<T> where T : BaseEntity
+	{
+		private readonly ApplicationDbContext _context;
+		private DbSet<T> _set;
+		public Repository(ApplicationDbContext context)
+		{
+			_context = context;
+			_set = _context.Set<T>();
+
+
+		}
+		public void Create(T entity)
+		{
+			if(entity==null)
+			{
+				throw new ArgumentNullException("entity");
+
+			}
+			_set.Add(entity);
+			
+		}
+
+		public void Delete(T entity)
+		{
+			if (entity == null)
+			{
+				throw new ArgumentNullException("entity");
+
+			}
+			
+			_set.Remove(entity);
+			
+		}
+
+		public void Delete(int id)
+		{
+			if(id == 0)
+			{
+				throw new ArgumentNullException("id");
+
+			}
+			var x = _set.SingleOrDefault(x => x.Id == id);
+
+			_set.Remove(x);
+	
+		}
+
+		public T Get(int id)
+		{
+			return _set.SingleOrDefault(x => x.Id == id);
+
+		}
+
+		public IEnumerable<T> GetAll()
+		{
+			return _set.AsEnumerable();
+		}
+
+		public void Update(T entity)
+		{
+			if (entity == null)
+			{
+				throw new ArgumentNullException("entity");
+			}
+			_set.Update(entity);
+			_context.SaveChanges();
+		}
+	}
+}
