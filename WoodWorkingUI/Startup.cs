32c75440142a01886b358a3df345f7352c0d12c7
barteksmolec12 <barteksmolec@o2.pl>
2020-11-25 20:55:27 +0100

    ConfigureServices (AddDbContext,AddScoped, AddTransient)

--- WoodWorkingUI/Startup.cs
+++ WoodWorkingUI/Startup.cs
@@ -5,9 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Repository;
+using Services;
+using Services.Abstract;
 
 namespace WoodWorkingUI
 {
@@ -24,8 +28,10 @@ namespace WoodWorkingUI
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
+			services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+			services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+			services.AddTransient<IProductService, ProductService>();
 		}
-
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
