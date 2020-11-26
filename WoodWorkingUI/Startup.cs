8ee9f60f5cb6d0f494043187f283e3fecaec6dbe
barteksmolec12 <barteksmolec@o2.pl>
2020-11-26 23:31:52 +0100

    Install RunTime Compilation

--- WoodWorkingUI/Startup.cs
+++ WoodWorkingUI/Startup.cs
@@ -27,6 +27,7 @@ namespace WoodWorkingUI
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddRazorPages().AddRazorRuntimeCompilation();
 			services.AddControllersWithViews();
 			services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 			services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
