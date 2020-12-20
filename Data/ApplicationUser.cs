7ebc05332cb59c07ff690ea3e854bc2607a8e873
barteksmolec12 <barteksmolec@o2.pl>
2020-12-20 09:29:34 +0100

    Set display name attribute for ApplicationUser model

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -2,18 +2,25 @@
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Data
 {
 	public class ApplicationUser : IdentityUser
 	{
+		[Display(Name = "Imię i nazwisko")]
 		public string Name { get; set; }
+		[Display(Name = "Ulica")]
 		public string StreetAddress { get; set; }
+		[Display(Name = "Miejscowość")]
 		public string City { get; set; }
-		public string State { get; set; }
+		[Display(Name = "Stan")]
+        public string State { get; set; }
+		[Display(Name = "Kod pocztowy")]
 		public string PostalCode { get; set; }
-
+		
+		[Display(Name = "Powtórz hasło")]
 		public string ConfirmPassword { get; set; }
 	}
 }
