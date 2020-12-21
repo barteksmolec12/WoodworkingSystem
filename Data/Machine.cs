df0ec644cb917da4495a52c4f86bdffed0b6834d
barteksmolec12 <barteksmolec@o2.pl>
2020-12-22 00:51:52 +0100

    Add user in Machine model

--- Data/Machine.cs
+++ Data/Machine.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Data
 {
 	public class Machine:BaseEntity
 	{
-		
+		public string Name { get; set; }
 		public string  Image { get; set; }
-		public int WoodmakerId { get; set; }
+		public string WoodmakerId { get; set; }
+		[ForeignKey("WoodmakerId")]
+		public virtual ApplicationUser ApplicationUser { get; set; }
 		public string State { get; set; }
 	}
 }
