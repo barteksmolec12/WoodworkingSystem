3f9600dceff641e49328a5e30b411d793b7d3b5a
barteksmolec12 <barteksmolec@o2.pl>
2020-12-30 23:46:37 +0100

    Finding actual logged user

--- JoineryUI/Controllers/PositionsStateController.cs
+++ JoineryUI/Controllers/PositionsStateController.cs
@@ -5,6 +5,7 @@ using Service.Abstract;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace JoineryUI.Controllers
@@ -23,6 +24,7 @@ namespace JoineryUI.Controllers
 		public IActionResult GetMachines()
 		{
 			var res = _machineService.GetAllWithInclude("ApplicationUser");
+
 			return Ok(res);
 		}
 		public IActionResult Index()
@@ -40,10 +42,17 @@ namespace JoineryUI.Controllers
 			if(m.State=="Wolne")
 			{
 				m.State = "Zajęte";
+
+				var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+				var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+				m.WoodmakerId = claim.Value;
+
 			}
 			else
 			{
 				m.State = "Wolne";
+				m.WoodmakerId = null;
 			}
 					
 
