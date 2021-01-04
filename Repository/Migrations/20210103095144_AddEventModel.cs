573de5d70342afeb4a6c4815a1edb84585f2d125
barteksmolec12 <barteksmolec@o2.pl>
2021-01-04 23:11:06 +0100

    Migrations

---
+++ Repository/Migrations/20210103095144_AddEventModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JoineryUI.Data.Migrations
+{
+    public partial class AddEventModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Subject = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: false),
+                    Start = table.Column<DateTime>(nullable: false),
+                    End = table.Column<DateTime>(nullable: true),
+                    ThemeColor = table.Column<string>(nullable: true),
+                    IsFullDay = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
---
+++ Repository/Migrations/20210103100041_RenamePropertiesInEventModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JoineryUI.Data.Migrations
+{
+    public partial class RenamePropertiesInEventModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "End",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Start",
+                table: "Events");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndAssembley",
+                table: "Events",
+                nullable: true,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartAssembley",
+                table: "Events",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndAssembley",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "StartAssembley",
+                table: "Events");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "End",
+                table: "Events",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Start",
+                table: "Events",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
