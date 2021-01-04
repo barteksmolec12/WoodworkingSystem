c857b99628533a6a44b62aa2f91bf0aed298a5ed
barteksmolec12 <barteksmolec@o2.pl>
2021-01-04 23:09:43 +0100

    Create event model

---
+++ Data/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Data
+{
+	public class Event:BaseEntity
+	{
+       [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public DateTime StartAssembley { get; set; }
+        public DateTime EndAssembley { get; set; }
+        public string ThemeColor { get; set; }
+        public bool IsFullDay { get; set; }
+
+    }
+}
